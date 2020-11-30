df2bb4615de191df51107494a98da9d31e2b601a
Kayla Raflores <kaylacr2@illinois.edu>
2020-11-29 20:47:36 -0600

    adjust search

--- templates/nextjs/pages/index.js
+++ templates/nextjs/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import React from 'react';
-import { Box, ThemeProvider, CSSReset } from '@chakra-ui/core';
 import Hero from '../components/Hero';
 import Container from '../components/Container';
 
