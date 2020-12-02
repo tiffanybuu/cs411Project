f52cd1c6056bd177f26d58710fa1226dc8e8aa47
Kayla Raflores <kaylacr2@illinois.edu>
2020-12-02 14:30:13 -0600

    see playlist

--- templates/nextjs/pages/user/[user].js
+++ templates/nextjs/pages/user/[user].js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Avatar, Box, Text, Divider } from '@chakra-ui/core';
+import { Avatar, Box, Text, Button, Divider } from '@chakra-ui/core';
 // import { Button, ButtonGroup } from "@chakra-ui/react";
 import NavigationButton from '../../components/NavigationButton';
 import HeaderGrid from '../../components/HeaderGrid';
@@ -9,18 +9,18 @@ import ResponsiveHeading from '../../components/ResponsiveHeading';
 import Container from '../../components/Container';
 import { RESPONSIVE_TEXT_ALIGN } from '../../styles/responsiveStyles';
 import { fetchUsers, fetchPlaylists, fetchEntries } from '../../clients';
-import { useRouter } from 'next/router';
+import Router, { withRouter, useRouter } from 'next/router';
 import fetch from 'node-fetch';
 import axios from "axios";
 
-
-const User = ({ user }) => {
+const User = ({ user, playlistsj }) => {
   var userId, firstName, lastName, followingCount, followerCount, playlists;
   userId = user.data.UserInfo.UserID;
   firstName = user.data.UserInfo.FirstName;
   lastName = user.data.UserInfo.LastName;
   followingCount = user.data.UserInfo.FollowingCount;
   followerCount = user.data.UserInfo.FollowerCount;
+  playlists = playlistsj.data.Playlists;
 
   const [show, setShow] = useState(false);
   const handleToggle = () => setShow(!show);
@@ -48,7 +48,33 @@ const User = ({ user }) => {
       <Container>
         {/* User's playlists */}
         <Box>
-          {/*<Playlists playlists={playlistId} tags={tags} users={users} />*/}
+          <table className="search-result-table">
+            <tbody>
+              <tr className='search-result-header'>
+                <td className='header-td'>Title</td>
+                <td className='header-td'>Description</td>
+                <td className='header-td'>Username</td>
+                <td className='header-td'>Date Created</td>
+              </tr>
+
+              {playlists.map((playlist) => {
+                return (
+                  <tr className='search-result-entry'>
+                    <td className='result-td'>
+                    <Link className='link-search' href={{pathname: '/playlists/', query: {userID: playlist.UserID, playlistID: playlist.PlaylistID}}}
+                    style = {{textDecorationLine: "none"}}
+                    >
+                      {playlist.Title}
+                      </Link>
+                    </td>
+                    <td className='result-td'>{playlist.Description}</td>
+                    <td className='result-td'>{playlist.UserID}</td>
+                    <td className='result-td'>{playlist.DateCreated.split('-').join(' ')}</td>
+                  </tr>
+                )
+            })}
+            </tbody>
+          </table>
           {!playlists && (
             <Text textAlign={RESPONSIVE_TEXT_ALIGN}>
               {`${firstName} hasn't made any playlists yet.`}
@@ -113,11 +139,15 @@ export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
   const MYURL = "http://127.0.0.1:5000";
   const username = params.user;
+
   const request = await fetch(`${MYURL}/get-user-info/${username}`);
   const user = await request.json();
 
+  const request2 = await fetch(`${MYURL}/get-playlists/${username}`);
+  const playlistsj = await request2.json();
+
   return {
-    props: { user }
+    props: { user, playlistsj }
   };
 }
 
